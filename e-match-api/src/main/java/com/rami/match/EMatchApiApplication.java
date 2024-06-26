0adf4bd296b21260b446103c5d3ff8df3101444a
CCRami <tabib.rami@esprit.tn>
2024-06-26 04:40:52 +0100

    init commit

---
+++ e-match-api/src/main/java/com/rami/match/EMatchApiApplication.java
@@ -0,0 +1,13 @@
+package com.rami.match;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EMatchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EMatchApiApplication.class, args);
+	}
+
+}
---
+++ e-match-api/src/test/java/com/rami/match/EMatchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rami.match;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EMatchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
